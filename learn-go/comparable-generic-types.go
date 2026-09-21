0ac135654ca202786dab44bd13d2079af7d94a2c
Jerry Chen <jerryschen@gmail.com>
2026-09-20 22:27:44 -0700

    comment updates

--- learn-go/comparable-generic-types.go
+++ learn-go/comparable-generic-types.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 )
 
+// comparable and any are more generic types that are used to create generic data structures and associated methods
+
 // Index returns the index of x in s, or -1 if not found.
 // T is any type that fulfills the built-in constraint "comparable" - which is types that have an == and a few other comparison operators
 // here, if T was of type "any" this would fail because this is too loose of a constraint and Go does not know if == is a safe operator on an "any" type
